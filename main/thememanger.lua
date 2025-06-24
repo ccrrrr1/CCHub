e0a52115c524bb988aa42a426b6a78b23b1ed5cb
ccrrrr1 <ccrrrr100@gmail.com>
2025-06-24 04:08:39 -0400

    Update thememanger.lua

--- main/thememanger.lua
+++ main/thememanger.lua
@@ -81,34 +81,6 @@ local ThemeManager = {} do
 		groupbox:AddLabel('Accent color'):AddColorPicker('AccentColor', { Default = self.Library.AccentColor });
 		groupbox:AddLabel('Outline color'):AddColorPicker('OutlineColor', { Default = self.Library.OutlineColor });
 		groupbox:AddLabel('Font color')	:AddColorPicker('FontColor', { Default = self.Library.FontColor });
-local FontNameToID = {
-    Legacy = 0, Arial = 1, ArialBold = 2, SourceSans = 3, SourceSansBold = 4,
-    SourceSansLight = 5, SourceSansItalic = 6, Bodoni = 7, Garamond = 8,
-    Cartoon = 9, Code = 10, Highway = 11, SciFi = 12, Arcade = 13, Fantasy = 14,
-    Antique = 15, SourceSansSemibold = 16, Gotham = 17, GothamMedium = 18,
-    GothamBold = 19, GothamBlack = 20, AmaticSC = 21, Bangers = 22,
-    Creepster = 23, DenkOne = 24, Fondamento = 25, FredokaOne = 26,
-    GrenzeGotisch = 27, IndieFlower = 28, JosefinSans = 29, Jura = 30,
-    Kalam = 31, LuckiestGuy = 32, Merriweather = 33, Michroma = 34,
-    Nunito = 35, Oswald = 36, PatrickHand = 37, PermanentMarker = 38,
-    Roboto = 39, RobotoCondensed = 40, RobotoMono = 41, Sarpanch = 42,
-    SpecialElite = 43, TitilliumWeb = 44, Ubuntu = 45, BuilderSans = 46,
-    BuilderSansMedium = 47, BuilderSansBold = 48, BuilderSansExtraBold = 49,
-    Arimo = 50, ArimoBold = 51, Unknown = 100
-}
-
-groupbox:AddDropdown('FontSelector', {
-    Text = 'Font',
-    Values = table.pack(unpack(table.keys(FontNameToID))),
-    Default = 1,
-    Callback = function(fontName)
-        local fontId = FontNameToID[fontName]
-        if fontId then
-            self.Library.Font = fontId
-        end
-    end
-})
-
 
 		local ThemesArray = {}
 		for Name, Theme in next, self.BuiltInThemes do
