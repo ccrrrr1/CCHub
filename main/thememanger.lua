b4697ce930fd119bf21e890349ef9b9715f6cda1
ccrrrr1 <ccrrrr100@gmail.com>
2025-09-01 06:12:34 -0400

    Update thememanger.lua

--- main/thememanger.lua
+++ main/thememanger.lua
@@ -45,7 +45,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
+        ['Default'] = { 1, httpService:JSONDecode('{"FontColor":"dcdcdc","MainColor":"1c1c1c","AccentColor":"5c5c5c","BackgroundColor":"101010","OutlineColor":"2e2e2e"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
