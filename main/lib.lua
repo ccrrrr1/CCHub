a1833a671e9a4b8ba944254b20ecfb86d6e20bc2
ccrrrr1 <ccrrrr100@gmail.com>
2025-09-01 04:27:59 -0400

    Update lib.lua

--- main/lib.lua
+++ main/lib.lua
@@ -120,7 +120,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 50, 50);
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Code,
+    Font = Enum.Font.Fantasy,
 
     -- frames --
     OpenedFrames = {};
